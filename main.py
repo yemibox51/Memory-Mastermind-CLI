0a9923e623c5af5748fdcf86dd56375155401b4a
yemibox51 <yemibox51@gmail.com>
2023-08-28 00:18:55 -0400

    Initial plan

---
+++ main.py
@@ -0,0 +1,12 @@
+import click
+
+def play():
+    # Generate board based on size, board will always be a box
+    # Randomly place stones (o) across board, placement and amount of stones will depend on difficulty
+    # Clear stones from board
+    # Render stone location when user enters one
+    # Validate stone locations (✓)(x)
+    pass
+
+if __name__ == "__main__":
+    play()
